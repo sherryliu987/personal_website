d0c6d2f651e37d93f8ca940a0747ed7534121585
Sherry <sherryliu987@gmail.com>
2020-11-17 22:48:19 -0500

    initial routes

---
+++ application/routes.py
@@ -0,0 +1,24 @@
+from flask import render_template
+
+from application import app
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
